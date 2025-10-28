c3b609dbf5232aceef611295712867c9bcef35a7
Pablo Chaves <chavespablo2605@gmail.com>
2025-10-27 19:05:45 -0600

    Creación de index de nuestra pagina

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set("America/Costa_Rica");
+$fecha = new DateTime();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>csgo.com</title>
+</head>
+<body>
+    <h1><?php echo "Bienvenido a CSGO, hoy es:",  $fecha ?> </h1>
+</body>
+</html>
